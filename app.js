aa8fc5e08f5586aa453c798fdc5fcc824a4ecdef
Gustavo Palandi <gustavoppo@hotmail.com>
2024-06-18 20:24:24 -0300

    chore: Update form placeholder and add location info display

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota para servir o arquivo HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Inicia o servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor Express rodando na porta ${PORT}`);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -9,34 +9,42 @@ const position = { lat: lat, lng: lng };
 async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
-  const { Marker } = await google.maps.importLibrary("marker"); // Use Marker instead of AdvancedMarkerElement
+  const { Marker } = await google.maps.importLibrary("marker");
 
   map = new Map(document.getElementById("map"), {
     center: position,
     zoom: 13,
   });
 
-  marker = new Marker({ // Create the marker after the map is initialized
+  marker = new Marker({
     position: position,
     map: map,
     title: "Pin",
   });
 }
 
-function updateMap(lat, lng) {
-  if (
-    typeof lat === "number" &&
-   !isNaN(lat) &&
-    typeof lng === "number" &&
-   !isNaN(lng)
-  ) {
-    const position = { lat: lat, lng: lng };
-    map.setCenter(position);
-    map.setZoom(19);
-    marker.setPosition(position); // Use setPosition instead of position
-  } else {
-    console.error("Invalid latitude or longitude");
-  }
+function updateMap(loc) {
+  const geocoder = new google.maps.Geocoder();
+  
+  let positionCep = { lat: 0, lng: 0 };
+
+  geocoder.geocode({ address: loc }, function (results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[0]) {
+        positionCep.lat = results[0].geometry.location.lat();
+        positionCep.lng = results[0].geometry.location.lng();
+        
+        document.getElementById('lat').textContent = 'Latitude: ' + positionCep.lat.toFixed(6);
+        document.getElementById('lng').textContent = 'Longitude: ' + positionCep.lng.toFixed(6);
+
+        map.setCenter(positionCep);
+        map.setZoom(19);
+        marker.setPosition(positionCep);
+      }
+    } else {
+      console.error('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -44,19 +52,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    const latInput = document.getElementById("latitude").value;
-    const lngInput = document.getElementById("longitude").value;
 
-    const newLat = parseFloat(latInput);
-    const newLng = parseFloat(lngInput);
+    const loc = document.getElementById("local").value;
 
-    if (!isNaN(newLat) &&!isNaN(newLng)) {
-      lat = newLat;
-      lng = newLng;
-      updateMap(lat, lng);
-    } else {
-      console.error("Invalid input: Latitude and Longitude must be numbers.");
-    }
+    updateMap(loc);
   });
 });
 
